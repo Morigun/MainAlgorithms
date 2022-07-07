dd648404cee2439f12403c02fea30027cccc1700
_sda_ <sda@eldis24.ru>
2022-07-07 13:03:01 +0300

    - Добавлены тесты для больших массивов

--- MainAlgorithmsTests/Sorting/ShakeTests.cs
+++ MainAlgorithmsTests/Sorting/ShakeTests.cs
@@ -38,5 +38,93 @@ namespace MainAlgorithms.Sorting.Tests
             new Shake(new StatService()).Sort(arr);
             Assert.IsTrue(TestData.CompareArrays(TestData.Exp10ArrAllSigns, arr));
         }
+        [TestMethod()]
+        public void Test1Positive()
+        {
+            var arr = TestData.Exp1ArrPositive.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp1ArrPositive, arr));
+        }
+        [TestMethod()]
+        public void Test1Negative()
+        {
+            var arr = TestData.Exp1ArrNegative.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp1ArrNegative, arr));
+        }
+        [TestMethod()]
+        public void Test100Positive()
+        {
+            var arr = TestData.Exp100ArrPositive.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp100ArrPositive, arr));
+        }
+        [TestMethod()]
+        public void Test100Negative()
+        {
+            var arr = TestData.Exp100ArrNegative.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp100ArrNegative, arr));
+        }
+        [TestMethod()]
+        public void Test100AllSigns()
+        {
+            var arr = TestData.Exp100ArrAllSigns.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp100ArrAllSigns, arr));
+        }
+        [TestMethod()]
+        public void Test1000Positive()
+        {
+            var arr = TestData.Exp1000ArrPositive.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp1000ArrPositive, arr));
+        }
+        [TestMethod()]
+        public void Test1000Negative()
+        {
+            var arr = TestData.Exp1000ArrNegative.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp1000ArrNegative, arr));
+        }
+        [TestMethod()]
+        public void Test1000AllSigns()
+        {
+            var arr = TestData.Exp1000ArrAllSigns.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp1000ArrAllSigns, arr));
+        }
+        [TestMethod()]
+        public void Test10000Positive()
+        {
+            var arr = TestData.Exp10000ArrPositive.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp10000ArrPositive, arr));
+        }
+        [TestMethod()]
+        public void Test10000Negative()
+        {
+            var arr = TestData.Exp10000ArrNegative.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp10000ArrNegative, arr));
+        }
+        [TestMethod()]
+        public void Test10000AllSigns()
+        {
+            var arr = TestData.Exp10000ArrAllSigns.ToList();
+            arr.Reverse();
+            new Shake(new StatService()).Sort(arr);
+            Assert.IsTrue(TestData.CompareArrays(TestData.Exp10000ArrAllSigns, arr));
+        }
     }
 }
--- MainAlgorithmsTests/TestData.cs
+++ MainAlgorithmsTests/TestData.cs
@@ -8,15 +8,43 @@ namespace MainAlgorithmsTests
 {
     internal class TestData
     {
+        enum TypeArray { POSITIVE, NEGATIVE, ALL_SIGNS }
+        public static List<int> Exp1ArrPositive = new List<int> { 0 };
+        public static List<int> Exp1ArrNegative = new List<int> { -1 };
         public static List<int> Exp10ArrPositive = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         public static List<int> Exp10ArrNegative = new List<int> { -9, -8, -7, -6, -5, -4, -3, -2, -1 };
         public static List<int> Exp10ArrAllSigns = new List<int> { -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public static List<int> Exp100ArrPositive => GetArray(100, TypeArray.POSITIVE).ToList();
+        public static List<int> Exp100ArrNegative => GetArray(100, TypeArray.NEGATIVE).ToList();
+        public static List<int> Exp100ArrAllSigns => GetArray(100, TypeArray.ALL_SIGNS).ToList();
+        public static List<int> Exp1000ArrPositive => GetArray(1000, TypeArray.POSITIVE).ToList();
+        public static List<int> Exp1000ArrNegative => GetArray(1000, TypeArray.NEGATIVE).ToList();
+        public static List<int> Exp1000ArrAllSigns => GetArray(1000, TypeArray.ALL_SIGNS).ToList();
+        public static List<int> Exp10000ArrPositive => GetArray(10000, TypeArray.POSITIVE).ToList();
+        public static List<int> Exp10000ArrNegative => GetArray(10000, TypeArray.NEGATIVE).ToList();
+        public static List<int> Exp10000ArrAllSigns => GetArray(10000, TypeArray.ALL_SIGNS).ToList();
+        public static List<int> Exp100000ArrPositive => GetArray(100000, TypeArray.POSITIVE).ToList();
+        public static List<int> Exp100000ArrNegative => GetArray(100000, TypeArray.NEGATIVE).ToList();
+        public static List<int> Exp100000ArrAllSigns => GetArray(100000, TypeArray.ALL_SIGNS).ToList();
         public static bool CompareArrays(List<int> arr1, List<int> arr2)
         {
             for (int i = 0; i < arr1.Count; i++)
                 if (arr1[i] != arr2[i])
                     return false;
             return true;
+        }
+        private static IEnumerable<int> GetArray(int size, TypeArray type)
+        {
+            if (type == TypeArray.POSITIVE)
+                for (int i = 0; i < size; i++)
+                    yield return i;
+            else if (type == TypeArray.NEGATIVE)
+                for (int i = -(size - 1); i < 0; i++)
+                    yield return i;
+            else
+                for (int i = -(size / 2 - 1); i < (size / 2); i++)
+                    yield return i;
+            yield break;
         }
     }
 }
